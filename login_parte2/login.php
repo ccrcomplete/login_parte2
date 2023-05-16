9025c3496d42b7bff2c08bb36fa26092251688cd
ccrcomplete <124693353+ccrcomplete@users.noreply.github.com>
2023-05-16 11:57:15 -0500

    Add files via upload
    New form created

---
+++ login_parte2/login.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="shortcut icon" href="./favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="./css/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body class="bg-body-tertiary">
+    <div class="container-fluid">
+        <div class="row justify-content-md-center">
+            <div class="col-md-5 d-flex justify-content-end">
+                <div class="animationSec col-md-2 align-items-start">
+                    <div class="backg">
+                        <div class="planet">
+                            <div class="r1"></div>
+                            <div class="r2"></div>
+                            <div class="r3"></div>
+                            <div class="r4"></div>
+                            <div class="r5"></div>
+                            <div class="r6"></div>
+                            <div class="r7"></div>
+                            <div class="r8"></div>
+                            <div class="shad"></div>
+                        </div>
+                            <div class="stars">
+                                <div class="s1"></div>
+                                <div class="s2"></div>
+                                <div class="s3"></div>
+                                <div class="s4"></div>
+                                <div class="s5"></div>
+                                <div class="s6"></div>
+                            </div>
+                    <div class="an">
+                        <div class="tank"></div>
+                        <div class="astro">
+                        <div class="helmet">
+                        <div class="glass">
+                <div class="shine"></div>
+                </div>
+            </div>
+                    <div class="dress">
+                        <div class="c">
+                        <div class="btn1"></div>
+                        <div class="btn2"></div>
+                        <div class="btn3"></div>
+                        <div class="btn4"></div>
+                    </div>
+                </div>
+                <div class="handl">
+                    <div class="handl1">
+                    <div class="glovel">
+                        <div class="thumbl"></div>
+                        <div class="b2"></div>
+                    </div>
+                    </div>
+                </div>
+                <div class="handr">
+                    <div class="handr1">
+                    <div class="glover">
+                        <div class="thumbr"></div>
+                        <div class="b1"></div>
+                    </div>
+                    </div>
+                </div>
+                <div class="legl">
+                    <div class="bootl1">
+                    <div class="bootl2"></div>
+                    </div>
+                </div>
+                <div class="legr">
+                    <div class="bootr1">
+                    <div class="bootr2"></div>
+                    </div>
+                </div>
+                <div class="pipe">
+                    <div class="pipe2">
+                    <div class="pipe3"></div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+                </div>
+                <div class="d-flex px-5 bg-body flex-column justify-content-center align-items-center vh-100">
+                    <h1 class="mb-3">LOGIN</h1>
+                    <div class="d-flex flex-column justify-content-center align-items-center">
+                    </div>
+                    <form class="d-flex flex-column w-75" method="POST">
+                        <div class="row mb-4">
+                            <div class="col">
+                                <label for="FirstName" class="form-label">Email</label>
+                                <input type="email" name="email" class="form-control" id="email" placeholder="example@gmail.com" aria-label="email">
+                            </div>
+                            <div class="col">
+                                <label for="LastName" class="form-label">Password</label>
+                                <input type="password" name="pass" id="pass" class="form-control" aria-label="pass">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col d-grid">
+                                <button type="submit" name="submit" class="btn btn-dark py-2 text-uppercase">Login</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+
+</html>
